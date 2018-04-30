6a5f27388c0953d0bed5bf3572f5178d8e3def17
Abdullahinur <aabdullahinur@hotmail.com>
2018-04-30 20:39:06 +0300

    generate project portfolio and startapp home

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/abdullahinir/Documents/moringa-core/Django-Developement/my-portfolio/virtual/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
